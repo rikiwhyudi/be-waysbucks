e47c4654ab4e535dfc89415d16619c6b25ffbd39
Lintah-Ngesot <ujangsuryana588@gmail.com>
2022-12-03 22:12:05 +0700

    be-waysbucks

---
+++ handlers/product.go
@@ -0,0 +1,208 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	productdto "waysbuck-API/dto/product"
+	dto "waysbuck-API/dto/result"
+	"waysbuck-API/models"
+	"waysbuck-API/repositories"
+
+	"context"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+type handlerProduct struct {
+	ProductRepository repositories.ProductRepository
+}
+
+func HandlerProduct(ProductRepository repositories.ProductRepository) *handlerProduct {
+	return &handlerProduct{ProductRepository}
+}
+
+func (h *handlerProduct) FindProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	products, err := h.ProductRepository.FindProducts()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "success", Data: products}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) GetProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	var product models.Product
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "success", Data: product}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dataContex := r.Context().Value("dataFile")
+	filepath := dataContex.(string)
+
+	price, _ := strconv.Atoi(r.FormValue("price"))
+	request := productdto.ProductRequest{
+		Title: r.FormValue("title"),
+		Price: price,
+		Image: filepath,
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	var ctx = context.Background()
+	var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+	var API_KEY = os.Getenv("API_KEY")
+	var API_SECRET = os.Getenv("API_SECRET")
+
+	// Add your Cloudinary credentials ...
+	cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+
+	// Upload file to Cloudinary ...
+	resp, err := cld.Upload.Upload(ctx, filepath, uploader.UploadParams{Folder: "waysbuck"})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	product := models.Product{
+		Title: request.Title,
+		Price: request.Price,
+		Image: resp.SecureURL,
+	}
+
+	// err := mysql.DB.Create(&product).Error
+	product, err = h.ProductRepository.CreateProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	product, _ = h.ProductRepository.GetProduct(product.ID)
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "success", Data: product}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	dataContex := r.Context().Value("dataFile")
+	filepath := dataContex.(string)
+
+	price, _ := strconv.Atoi(r.FormValue("price"))
+
+	request := productdto.ProductRequest{
+		Title: r.FormValue("title"),
+		Price: price,
+		Image: filepath,
+	}
+
+	validation := validator.New()
+	err := validation.Struct(request)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	var ctx = context.Background()
+	var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+	var API_KEY = os.Getenv("API_KEY")
+	var API_SECRET = os.Getenv("API_SECRET")
+
+	// Add your Cloudinary credentials ...
+	cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+
+	resp, err := cld.Upload.Upload(ctx, filepath, uploader.UploadParams{Folder: "waysbuck"})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	product, _ := h.ProductRepository.GetProduct(id)
+	product.Title = request.Title
+	product.Price = request.Price
+	product.Image = resp.SecureURL
+
+	if filepath != "false" {
+		product.Image = resp.SecureURL
+	}
+
+	product, err = h.ProductRepository.UpdateProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "success", Data: product}
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *handlerProduct) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	product, err := h.ProductRepository.GetProduct(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := dto.ErrorResult{Code: http.StatusBadRequest, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	data, err := h.ProductRepository.DeleteProduct(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := dto.ErrorResult{Code: http.StatusInternalServerError, Message: err.Error()}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response := dto.SuccessResult{Status: "success", Data: data}
+	json.NewEncoder(w).Encode(response)
+}
